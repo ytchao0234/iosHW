fada977afbbb5dadc5c45da06724ce006df29f70
ytchao0234 <nd68319@gmail.com>
2021-11-03 16:40:34 +0800

    HW2 init
    Signed-off-by: ytchao0234 <nd68319@gmail.com>

---
+++ HW2-EBook/AnotherEden/AnotherEden/AnotherEdenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherEdenApp.swift
+//  AnotherEden
+//
+//  Created by User10 on 2021/11/3.
+//
+
+import SwiftUI
+
+@main
+struct AnotherEdenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HW2-EBook/AnotherEden/AnotherEden/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnotherEden
+//
+//  Created by User10 on 2021/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
