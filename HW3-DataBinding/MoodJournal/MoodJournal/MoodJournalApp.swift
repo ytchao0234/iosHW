ebfc7fb4fa4f46891c6fe4216cbe8bfad057ad1a
ytchao0234 <nd68319@gmail.com>
2021-11-16 17:16:24 +0800

    init HW3
    Signed-off-by: ytchao0234 <nd68319@gmail.com>

---
+++ HW3-DataBinding/MoodJournal/MoodJournal/MoodJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodJournalApp.swift
+//  MoodJournal
+//
+//  Created by FanRende on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct MoodJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
