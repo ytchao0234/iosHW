79d68856941cda55c226084832c53f47931fdc06
ytchao0234 <nd68319@gmail.com>
2022-01-12 00:49:31 +0800

    feature: Novel Menu View [done], Novel Intro View [doing].
    Signed-off-by: ytchao0234 <nd68319@gmail.com>

---
+++ FinalProject-Part1/NovelReader/views/NovelView.swift
@@ -0,0 +1,20 @@
+//
+//  NovelView.swift
+//  NovelReader
+//
+//  Created by FanRende on 2022/1/11.
+//
+
+import SwiftUI
+
+struct NovelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NovelView_Previews: PreviewProvider {
+    static var previews: some View {
+        NovelView()
+    }
+}
---
+++ FinalProject-Part1/NovelReader/views/RatingView.swift
@@ -0,0 +1,20 @@
+//
+//  RatingView.swift
+//  NovelReader
+//
+//  Created by FanRende on 2022/1/11.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView()
+    }
+}
